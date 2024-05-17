f51c11c8e548bc2f440bfbd0c4b88179f66e1041
unknown <crsthian.04@gmail.com>
2024-05-16 22:25:08 -0300

    MANIPULAÇÃO DE STRING 3

--- STRING COM ESPAÇO.c
+++ STRING COM ESPAÇO.c
@@ -1,3 +1,9 @@
+// A biblioteca string.h contém uma série de funções para manipular strings. Por exemplo:
+// Copiar string origem para a string destino com strcpy(destino, origem)
+// Concatenar string2 no fim da string1 com strcat(string1, string2)
+// Descobrir o tamanho de uma string com strlen(stringX)
+// Comparar strings alfabeticamente com strcmp(string1, string2)
+
 #include <stdio.h>
 
 int main(){
